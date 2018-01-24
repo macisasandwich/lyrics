c3cc0e5647c62a9d47c2a16953787c46bfc64d48
Jesse Zhang <macisasandwich@gmail.com>
2018-01-24 13:00:00 -0600

    lol

--- get_songs_for_artist.py
+++ get_songs_for_artist.py
@@ -6,6 +6,5 @@ g = Grab()
 resp = g.go('https://www.azlyrics.com/e/eminem.html')
 soup = BeautifulSoup(resp.body, 'html.parser')
 
-print("hello")
-# with open('out', 'w') as f:
-#     f.write()
+with open('out', 'w') as f:
+    f.write(str(soup.body.prettify()))
